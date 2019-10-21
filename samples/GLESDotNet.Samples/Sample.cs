2d3266c80f543a676a996d88ccab95eda734358a
smack0007 <zachary.snow@gmail.com>
2019-10-21 21:51:04 +0200

    Factored out base class for samples.

---
+++ samples/GLESDotNet.Samples/Sample.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Diagnostics;
+using static GLFWDotNet.GLFW;
+using static GLESDotNet.EGL;
+using static GLESDotNet.GLES2;
+using GLFWDotNet.Utilities;
+using System.IO;
+using System.Reflection;
+
+namespace GLESDotNet.Samples
+{
+    public abstract class Sample : IDisposable
+    {
+        private const float TimeBetweenFrames = 1000.0f / 60.0f;
+
+        private IntPtr _display;
+        private IntPtr _surface;
+
+        private Stopwatch? _stopwatch;
+        private float _lastElapsed;
+        private float _elapsedSinceLastFrame;
+
+        private float _fpsElapsed;
+
+        public Window Window { get; }
+
+        public Keyboard Keyboard { get; }
+
+        public int FramesPerSecond { get; private set; }
+
+        public Sample()
+        {
+            var basePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            Directory.SetCurrentDirectory(basePath);
+
+            if (!Application.Init())
+                throw new InvalidOperationException("Failed to initialize GLFW.");
+
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+            Window = new Window();
+            CreateContext();
+
+            Keyboard = new Keyboard(Window);
+        }
+
+        ~Sample()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private unsafe void CreateContext()
+        {
+            eglInit();
+
+            _display = eglGetDisplay((IntPtr)EGL_DEFAULT_DISPLAY);
+
+            int majorVersion, minorVersion;
+            if (!eglInitialize(_display, &majorVersion, &minorVersion))
+            {
+                int error = eglGetError();
+                throw new InvalidOperationException();
+            }
+
+            eglBindAPI(EGL_OPENGL_ES_API);
+            if (eglGetError() != EGL_SUCCESS)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int[] configAttributes = new int[]
+            {
+                EGL_RED_SIZE, 8,
+                EGL_GREEN_SIZE, 8,
+                EGL_BLUE_SIZE, 8,
+                EGL_ALPHA_SIZE, 8,
+                EGL_DEPTH_SIZE, 24,
+                EGL_STENCIL_SIZE, 8,
+                EGL_SAMPLE_BUFFERS, EGL_DONT_CARE,
+                EGL_NONE
+            };
+
+            IntPtr config;
+            int configCount;
+            fixed (int* configAttributesPtr = configAttributes)
+            {
+                if (!eglChooseConfig(_display, configAttributesPtr, &config, 1, &configCount) || (configCount != 1))
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+
+            int[] surfaceAttributes = new int[]
+            {
+                EGL_NONE, EGL_NONE,
+            };
+
+            fixed (int* surfaceAttribtuesPtr = surfaceAttributes)
+            {
+                _surface = eglCreateWindowSurface(_display, config, Window.GetNativeHandle(), surfaceAttribtuesPtr);
+            }
+
+            if (_surface == IntPtr.Zero)
+            {
+                eglGetError(); // Clear error and try again
+                _surface = eglCreateWindowSurface(_display, config, IntPtr.Zero, null);
+            }
+
+            if (eglGetError() != EGL_SUCCESS)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int[] contextAttibutes = new int[]
+            {
+                EGL_CONTEXT_CLIENT_VERSION, 2,
+                EGL_NONE
+            };
+
+            IntPtr context;
+            fixed (int* contextAttributesPtr = contextAttibutes)
+            {
+                context = eglCreateContext(_display, config, IntPtr.Zero, contextAttributesPtr);
+                if (eglGetError() != EGL_SUCCESS)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+
+            eglMakeCurrent(_display, _surface, _surface, context);
+            if (eglGetError() != EGL_SUCCESS)
+            {
+                throw new InvalidOperationException();
+            }
+
+            // Turn off vsync
+            eglSwapInterval(_display, 0);
+
+            glInit(eglGetProcAddress);
+
+            //string title = "RENDERER: " + Marshal.PtrToStringAnsi(glGetString(GL_RENDERER)) + " ";
+            //title += "VENDOR: " + Marshal.PtrToStringAnsi(glGetString(GL_VENDOR)) + " ";
+            //title += "VERSION: " + Marshal.PtrToStringAnsi(glGetString(GL_VERSION));
+            //text += "EXTENSIONS: " + Environment.NewLine + glGetString(GL_EXTENSIONS);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            Application.Terminate();
+        }
+
+        public void Run()
+        {
+            _stopwatch = Stopwatch.StartNew();
+
+            Initialize();
+
+            Application.Run(Window, () =>
+            {
+                if (Keyboard[Keys.Escape])
+                {
+                    Window.Close();
+                    return;
+                }
+
+                Tick();
+            });
+        }
+
+        private void Tick()
+        {
+            float currentElapsed = (float)_stopwatch.Elapsed.TotalMilliseconds;
+            float deltaElapsed = currentElapsed - _lastElapsed;
+            _elapsedSinceLastFrame += deltaElapsed;
+            _lastElapsed = currentElapsed;
+
+            bool shouldDraw = _elapsedSinceLastFrame >= TimeBetweenFrames;
+
+            while (_elapsedSinceLastFrame >= TimeBetweenFrames)
+            {
+                Update(_elapsedSinceLastFrame);
+                _elapsedSinceLastFrame -= TimeBetweenFrames;
+            }
+
+            if (shouldDraw)
+            {
+                Draw();
+                eglSwapBuffers(_display, _surface);
+                FramesPerSecond++;
+            }
+
+            _fpsElapsed += deltaElapsed;
+
+            if (_fpsElapsed >= 1000.0f)
+            {
+                _fpsElapsed -= 1000.0f;
+                FramesPerSecond = 0;
+            }
+        }
+
+        protected virtual void Initialize()
+        {
+        }
+
+        protected virtual void Update(float elapsed)
+        {
+        }
+
+        protected virtual void Draw()
+        {
+        }
+
+        protected virtual void Shutdown()
+        {
+        }
+    }
+}
---
+++ samples/HelloTriangle/HelloTriangleSample.cs
@@ -0,0 +1,153 @@
+﻿using GLFWDotNet.Utilities;
+using System;
+using static GLESDotNet.GLES2;
+using System.Text;
+using GLESDotNet.Samples;
+
+namespace HelloTriangle
+{
+    public unsafe class HelloTriangleSample : Sample
+    {
+        private static uint _program;
+
+        public static void Main(string[] args)
+        {
+            using (var sample = new HelloTriangleSample())
+                sample.Run();
+        }        
+
+        public HelloTriangleSample()
+        {
+            Window.Title = "Hello Triangle";
+        }
+
+        private static uint CompileShader(string shaderSrc, uint type)
+        {
+            var shader = glCreateShader(type);
+            
+            if(shader == 0)
+                return 0;
+
+            var shaderSrcTmp = new string[] { shaderSrc };
+            var shaderLength = shaderSrc.Length;
+            glShaderSource(shader, 1, shaderSrcTmp, &shaderLength);
+
+            glCompileShader(shader);
+
+            int compiled;
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+            
+            if (compiled == 0)
+            {
+                int infoLength;
+                glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLength);
+
+                if (infoLength > 1)
+                {
+                    var infoLog = new StringBuilder(infoLength);
+                    glGetShaderInfoLog(shader, infoLength, null, infoLog);
+                    glDeleteShader(shader);
+                    throw new InvalidOperationException($"Error compiling shader:\n{infoLog}");
+                }
+            }
+            
+            return shader;
+        }
+
+        protected override void Initialize()
+        {
+            string vShaderStr =
+@"attribute vec3 vPosition;
+attribute vec3 vColor;
+
+varying vec3 fragColor;
+
+void main()
+{
+    gl_Position = vec4(vPosition, 1.0);
+    fragColor = vColor;
+}";
+
+            string fShaderStr =
+@"precision mediump float;
+
+varying vec3 fragColor;
+
+void main()
+{
+    gl_FragColor = vec4(fragColor, 1.0);
+}";
+            
+            uint vertexShader = CompileShader(vShaderStr, GL_VERTEX_SHADER);
+            uint fragmentShader = CompileShader(fShaderStr, GL_FRAGMENT_SHADER);
+            
+            _program = glCreateProgram();
+            if (_program == 0)
+                throw new InvalidOperationException("Failed to create program.");
+
+            glAttachShader(_program, vertexShader);
+            glAttachShader(_program, fragmentShader);
+            
+            glBindAttribLocation(_program, 0, "vPosition");
+            glBindAttribLocation(_program, 1, "vColor");
+            glLinkProgram(_program);
+
+            int linked;
+            glGetProgramiv(_program, GL_LINK_STATUS, &linked);
+            
+            if (linked == 0)
+            {
+                int infoLength;
+                glGetProgramiv(_program, GL_INFO_LOG_LENGTH, &infoLength);
+
+                if (infoLength > 1)
+                {
+                    var infoLog = new StringBuilder(infoLength);
+                    glGetProgramInfoLog(_program, infoLength, null, infoLog);
+                    glDeleteProgram(_program);
+                    throw new InvalidOperationException($"Error linking program:\n{infoLog}");
+                }
+            }
+
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        }
+
+        protected override void Draw()
+        {
+            float[] vVertices = new float[]
+            {
+                0.0f, 0.5f, 0.0f,
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f
+            };
+
+            float[] vColors = new float[]
+            {
+                1.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f,
+                0.0f, 0.0f, 1.0f
+            };
+
+            glViewport(0, 0, Window.Width, Window.Height);
+            glClear(GL_COLOR_BUFFER_BIT);
+            
+            glUseProgram(_program);
+
+            fixed (void* vVerticesPtr = vVertices)
+            {
+                glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, vVerticesPtr);
+            }
+
+            glEnableVertexAttribArray(0);
+
+            fixed (void* vColorsPtr = vColors)
+            {
+                glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, vColorsPtr);
+            }
+            
+            glEnableVertexAttribArray(1);
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        }
+    }
+}
