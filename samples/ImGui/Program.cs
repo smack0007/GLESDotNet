062ba4c63b5edae8665e1502dcacf13c9cd58b61
smack0007 <zachary.snow@gmail.com>
2020-10-10 22:48:54 +0200

    ImGui sample can render the demo window.

--- samples/ImGui/Program.cs
+++ samples/ImGui/Program.cs
@@ -12,8 +12,16 @@ namespace ImGuiGLESDotNet
     public unsafe class ImGuiSample : Sample
     {
         private uint _program;
+        private int _vertProjectionLocation;
         private int _fragTextureLocation;
         private uint _texture;
+
+        private const int VertexSizeInBytes = 20;
+        private const int VertexSizeInFloats = VertexSizeInBytes / sizeof(float);
+
+        private float[] _vertexData = new float[1024 * 1024];
+        private ushort[] _indexData = new ushort[1024 * 1024];
+
 
         public static void Main(string[] args)
         {
@@ -84,31 +92,33 @@ namespace ImGuiGLESDotNet
         protected override void Initialize()
         {
             string vertShader =
-@"attribute vec3 vertPosition;
-attribute vec3 vertColor;
+@"attribute vec2 vertPosition;
 attribute vec2 vertTexCoord;
+attribute vec4 vertColor;
 
-varying vec3 fragColor;
 varying vec2 fragTexCoord;
+varying vec4 fragColor;
+
+uniform mat4 vertProjection; 
 
 void main()
 {
-    gl_Position = vec4(vertPosition, 1.0);
-    fragColor = vertColor;
+    gl_Position = vertProjection * vec4(vertPosition, 0, 1);
     fragTexCoord = vertTexCoord;
+    fragColor = vertColor;
 }";
 
             string fragShader =
 @"precision mediump float;
 
-varying vec3 fragColor;
 varying vec2 fragTexCoord;
+varying vec4 fragColor;
 
 uniform sampler2D fragTexture;
 
 void main()
 {
-    gl_FragColor = texture2D(fragTexture, fragTexCoord);
+    gl_FragColor = fragColor * texture2D(fragTexture, fragTexCoord);
 }";
 
             uint vertexShader = CompileShader(vertShader, GL_VERTEX_SHADER);
@@ -122,10 +132,11 @@ void main()
             glAttachShader(_program, fragmentShader);
 
             glBindAttribLocation(_program, 0, "vertPosition");
-            glBindAttribLocation(_program, 1, "vertColor");
-            glBindAttribLocation(_program, 2, "vertTexCoord");
+            glBindAttribLocation(_program, 1, "vertTexCoord");
+            glBindAttribLocation(_program, 2, "vertColor");
             LinkProgram(_program);
 
+            _vertProjectionLocation = glGetUniformLocation(_program, "vertProjection");
             _fragTextureLocation = glGetUniformLocation(_program, "fragTexture");
 
             uint texture;
@@ -146,7 +157,7 @@ void main()
 
             fixed (byte* pixelsPtr = pixels)
             {
-                glTexImage2D(GL_TEXTURE_2D, 0, (int)GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, (void*)pixelsPtr);
+                glTexImage2D(GL_TEXTURE_2D, 0, (int)GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelsPtr);
             }
 
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (int)GL_LINEAR);
@@ -160,6 +171,13 @@ void main()
             glEnableVertexAttribArray(0);
             glEnableVertexAttribArray(1);
             glEnableVertexAttribArray(2);
+
+            glEnable(GL_BLEND);
+            glBlendEquation(GL_FUNC_ADD);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glDisable(GL_CULL_FACE);
+            glDisable(GL_DEPTH_TEST);
+            glEnable(GL_SCISSOR_TEST);
         }
 
         private float _elapsed;
@@ -173,12 +191,15 @@ void main()
         {
             var io = ImGui.GetIO();
             io.DeltaTime = _elapsed;
-            io.DisplaySize = new Vector2(WindowWidth, WindowHeight);
+            io.DisplaySize = new Vector2(800, 600);
             io.DisplayFramebufferScale = Vector2.One;
 
             ImGui.NewFrame();
 
-            ImGui.Text("Hello World!");
+            //ImGui.Begin("Hello World!");
+            //ImGui.Text("Text");
+            //ImGui.End();
+            ImGui.ShowUserGuide();
 
             ImGui.Render();
 
@@ -191,76 +212,78 @@ void main()
             {
                 ImDrawListPtr cmdList = drawData.CmdListsRange[n];
 
-                //fixed (void* vtxDstPtr = &_vertexData[vertexOffset * DrawVertDeclaration.Size])
-                //fixed (void* idxDstPtr = &_indexData[indexOffset * sizeof(ushort)])
-                //{
-                //    Buffer.MemoryCopy((void*)cmdList.VtxBuffer.Data, vtxDstPtr, _vertexData.Length, cmdList.VtxBuffer.Size * DrawVertDeclaration.Size);
-                //    Buffer.MemoryCopy((void*)cmdList.IdxBuffer.Data, idxDstPtr, _indexData.Length, cmdList.IdxBuffer.Size * sizeof(ushort));
-                //}
+                fixed (void* vertexDataPtr = &_vertexData[vertexOffset * VertexSizeInFloats])
+                fixed (void* indexDataPtr = &_indexData[indexOffset * sizeof(ushort)])
+                {
+                    Buffer.MemoryCopy(
+                        (void*)cmdList.VtxBuffer.Data,
+                        vertexDataPtr,
+                        _vertexData.Length * sizeof(float),
+                        cmdList.VtxBuffer.Size * VertexSizeInFloats * sizeof(float));
+
+                    Buffer.MemoryCopy(
+                        (void*)cmdList.IdxBuffer.Data,
+                        indexDataPtr,
+                        _indexData.Length * sizeof(ushort),
+                        cmdList.IdxBuffer.Size * sizeof(ushort));
+                }
 
                 vertexOffset += cmdList.VtxBuffer.Size;
                 indexOffset += cmdList.IdxBuffer.Size;
             }
-
-
-            float[] vertPositions = new float[]
-            {
-                -0.5f, 0.5f, 0.0f,
-                0.5f, 0.5f, 0.0f,
-                -0.5f, -0.5f, 0.0f,
-
-                0.5f, 0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f,
-                -0.5f, -0.5f, 0.0f,
-            };
-
-            float[] vertColors = new float[]
-            {
-                1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, 1.0f,
-
-                1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, 1.0f
-            };
-
-            float[] vertTexCoords = new float[]
-            {
-                0.0f, 0.0f,
-                1.0f, 0.0f,
-                0.0f, 1.0f,
-
-                1.0f, 0.0f,
-                1.0f, 1.0f,
-                0.0f, 1.0f
-            };
 
             glViewport(0, 0, WindowWidth, WindowHeight);
             glClear(GL_COLOR_BUFFER_BIT);
 
             glUseProgram(_program);
 
-            fixed (void* vertPositionsPtr = vertPositions)
+            fixed (float* vertexDataPtr = _vertexData)
             {
-                glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, vertPositionsPtr);
-            }
-
-            fixed (void* vertColorsPtr = vertColors)
-            {
-                glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, vertColorsPtr);
-            }
-
-            fixed (void* vertTexCoordsPtr = vertTexCoords)
-            {
-                glVertexAttribPointer(2, 2, GL_FLOAT, false, 0, vertTexCoordsPtr);
+                glVertexAttribPointer(0, 2, GL_FLOAT, false, VertexSizeInBytes, (byte*)vertexDataPtr);
+                glVertexAttribPointer(1, 2, GL_FLOAT, false, VertexSizeInBytes, (byte*)vertexDataPtr + 8);
+                glVertexAttribPointer(2, 4, GL_UNSIGNED_BYTE, true, VertexSizeInBytes, (byte*)vertexDataPtr + 16);
             }
 
             glActiveTexture(GL_TEXTURE0);
             glBindTexture(GL_TEXTURE_2D, _texture);
             glUniform1i(_fragTextureLocation, 0);
 
-            glDrawArrays(GL_TRIANGLES, 0, 6);
+            for (int n = 0; n < drawData.CmdListsCount; n++)
+            {
+                ImDrawListPtr cmdList = drawData.CmdListsRange[n];
+
+                for (int cmdi = 0; cmdi < cmdList.CmdBuffer.Size; cmdi++)
+                {
+                    ImDrawCmdPtr drawCmd = cmdList.CmdBuffer[cmdi];
+
+                    float L = drawData.DisplayPos.X;
+                    float R = drawData.DisplayPos.X + drawData.DisplaySize.X;
+                    float T = drawData.DisplayPos.Y;
+                    float B = drawData.DisplayPos.Y + drawData.DisplaySize.Y;
+
+                    float[] projection = new float[]
+                    {
+                        2.0f/(R-L),   0.0f,         0.0f,   0.0f,
+                        0.0f,         2.0f/(T-B),   0.0f,   0.0f,
+                        0.0f,         0.0f,        -1.0f,   0.0f,
+                        (R+L)/(L-R),  (T+B)/(B-T),  0.0f,   1.0f,
+                    };
+
+                    fixed (float* projectionPtr = projection)
+                    {
+                        glUniformMatrix4fv(_vertProjectionLocation, 1, false, projectionPtr);
+                    }
+
+                    fixed (ushort* indexDataPtr = _indexData)
+                    {
+                        glDrawElements(
+                            GL_TRIANGLES,
+                            (int)drawCmd.ElemCount,
+                            GL_UNSIGNED_SHORT,
+                            indexDataPtr);
+                    }
+                }
+            }
         }
     }
 }
